8bb2c1ffd54dd149d38d9d73418739774a11ee24
migromarj <miguelromeroarjona@gmail.com>
2023-01-25 12:28:48 +0100

    test(#012): Add GoogleCountryTrend model tests

--- main/tests_google_trends.py
+++ main/tests_google_trends.py
@@ -104,3 +104,61 @@ class GoogleTrendsTestCase(TestCase):
         self.assertEqual(GoogleTrend.objects.count(), 1)
         self.google_trend.delete()
         self.assertEqual(GoogleTrend.objects.count(), 0)
+
+class GoogleCountryTrendModelTestCase(TestCase):
+
+    def setUp(self):
+        self.country = Country.objects.create(name='Brazil', native_name='Brasil', acronym='BR', flag='https://flagcdn.com/br.svg', woeid=455189, pn='brazil')
+        self.google_country_trend = GoogleCountryTrend.objects.create(country=self.country)
+        self.google_trend = GoogleTrend.objects.create(name='Trend', country_trend=self.google_country_trend)
+
+
+    ###############################################
+    ### GoogleCountryTrend model creation tests ###
+    ###############################################
+
+    def test_correct_google_country_trend_model_creation(self):
+
+        self.assertEqual(GoogleCountryTrend.objects.count(), 1)
+        self.assertEqual(self.google_country_trend.country, self.country)
+        self.assertTrue(isinstance(self.google_country_trend, GoogleCountryTrend))
+        self.assertEqual(self.google_country_trend.__str__(), self.country.name)
+
+    # 'country' field
+
+    def test_incorrect_google_country_trend_model_creation_without_country(self):
+        with self.assertRaises(Exception):
+            GoogleCountryTrend.objects.create(country=None)
+
+    ##############################################
+    ### GoogleCountryTrend model update tests ###
+    ##############################################
+
+    def test_correct_google_country_trend_model_update(self):
+
+        country = Country.objects.create(name='Argentina', native_name='Argentina', acronym='AR', flag='https://flagcdn.com/ar.svg', woeid=332471, pn='argentina')
+
+        self.google_country_trend.country = country
+        self.google_country_trend.save()
+
+        self.assertEqual(GoogleCountryTrend.objects.count(), 1)
+        self.assertEqual(self.google_country_trend.country, country)
+
+    # 'country' field
+
+    def test_incorrect_google_country_trend_model_update_without_country(self):
+        with self.assertRaises(Exception):
+            self.google_country_trend.country = None
+            self.google_country_trend.save()
+
+    ##############################################
+    ### GoogleCountryTrend model delete tests ###
+    ##############################################
+
+    def test_correct_google_country_trend_model_delete(self):
+
+        self.assertEqual(GoogleCountryTrend.objects.count(), 1)
+        self.assertEqual(GoogleTrend.objects.count(), 1)
+        self.google_country_trend.delete()
+        self.assertEqual(GoogleCountryTrend.objects.count(), 0)
+        self.assertEqual(GoogleTrend.objects.count(), 0)
