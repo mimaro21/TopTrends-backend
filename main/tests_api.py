40f96eef33576156792f14e79dcc56eb570c5be1
migromarj <miguelromeroarjona@gmail.com>
2023-01-28 13:34:44 +0100

    test(#015): Add Google Trends query tests

--- main/tests_api.py
+++ main/tests_api.py
@@ -104,3 +104,75 @@ class TwitterTrendsTest(TestCase):
         result = schema.execute(query)
         self.assertIsNone(result.errors)
         self.assertEqual(len(result.data['countryTwitterTrends']), 0)
+
+class GoogleTrendsTest(TestCase):
+
+    def test_correct_country_defined_trends_number(self):
+
+        query = """
+            query{
+                countryGoogleTrends(country:"United States of America", trendsNumber:10){
+                    id,
+                    name
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryGoogleTrends']), 10)
+
+        # Make the same query when the result is found in the database
+
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryGoogleTrends']), 10)
+
+    def test_correct_country_undefined_trends_number(self):
+
+        query = """
+            query{
+                countryGoogleTrends(country:"United States of America"){
+                    id,
+                    name
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryGoogleTrends']), 5)
+
+    def test_correct_country_big_trends_number(self):
+
+        query = """
+            query{
+                countryGoogleTrends(country:"United States of America", trendsNumber:500){
+                    id,
+                    name
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryGoogleTrends']), 20)
+
+    def test_unknown_country(self):
+
+        query = """
+            query{
+                countryGoogleTrends(country:"Not country"){
+                    id,
+                    name
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryGoogleTrends']), 0)
