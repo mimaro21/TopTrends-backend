7d7ca991504d297c161aed56f1ef29048ba2a6b9
migromarj <miguelromeroarjona@gmail.com>
2023-01-28 13:12:57 +0100

    test(#015): Add Twitter trend query tests

--- main/tests_api.py
+++ main/tests_api.py
@@ -24,3 +24,83 @@ class CountriesTest(TestCase):
         result = schema.execute(query)
         self.assertIsNone(result.errors)
         self.assertEqual(len(result.data['allCountries']), 250)
+
+class TwitterTrendsTest(TestCase):
+
+    def test_correct_country_defined_trends_number(self):
+
+        query = """
+            query{
+                countryTwitterTrends(country: "Spain", trendsNumber:10){
+                    id,
+                    name,
+                    url,
+                    tweetVolume
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryTwitterTrends']), 10)
+
+        # Make the same query when the result is found in the database
+
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryTwitterTrends']), 10)
+
+    def test_correct_country_undefined_trends_number(self):
+
+        query = """
+            query{
+                countryTwitterTrends(country: "Spain"){
+                    id,
+                    name,
+                    url,
+                    tweetVolume
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryTwitterTrends']), 5)
+
+    def test_correct_country_big_trends_number(self):
+
+        query = """
+            query{
+                countryTwitterTrends(country: "Spain", trendsNumber:500){
+                    id,
+                    name,
+                    url,
+                    tweetVolume
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryTwitterTrends']), 25)
+
+    def test_unknown_country(self):
+
+        query = """
+            query{
+                countryTwitterTrends(country: "Not country"){
+                    id,
+                    name,
+                    url,
+                    tweetVolume
+                }
+            }
+        """
+
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        self.assertIsNone(result.errors)
+        self.assertEqual(len(result.data['countryTwitterTrends']), 0)
